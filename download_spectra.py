4ee7754cf6a3c0db4518b0f15b63820a4a30e63d
coding-cat-cosmo <huntmatt524@gmail.com>
2021-03-05 12:22:06 -0800

    added inial data retrieval of project

---
+++ download_spectra.py
@@ -0,0 +1,20 @@
+#import numpy as np
+#in_arr = [2, 0, 25]
+#in_arr = np.invert(in_arr)
+#print(in_arr)
+#outf.close()
+import os
+#from pathlib import Path
+#p = Path(os.getcwd())
+#gets parent path
+#base_directory = p.parent
+# Parent Directory path
+#parent_dir = str(p.parent)
+#place = os.path.join(parent_dir, "")
+os.chdir("../../test")
+print(os.getcwd())
+os.chdir("../dr12q/spectra")
+print(os.getcwd())
+os.system("rsync --info=progress2 -h --no-motd --files-from=file_list rsync://data.sdss.org/dr12/boss/spectro/redux/ . 2> /dev/null")
+#print(os.system("rsync --info=progress2 -h --no-motd --files-from=file_list rsync://data.sdss.org/dr12/boss/spectro/redux/ . 2> /dev/null"))
+
